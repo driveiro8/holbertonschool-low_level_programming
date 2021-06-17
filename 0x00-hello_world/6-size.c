827202d3ba476c0359f8016a751e7053bd27cb50
Diego Riveiro <driveiro8@gmail.com>
2021-06-17 13:23:31 -0700

    C

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -9,12 +9,12 @@ char charType;
 int intType;
 long int longType;
 long long int longlongType;
-float floatType; 
+float floatType;
 printf("Size of a char: %zu byte(s)\n", sizeof(charType));
 printf("Size of an int: %zu byte(s)\n", sizeof(intType));
 printf("Size of a long int: %zu byte(s)\n", sizeof(longType));
 printf("Size of a long long int: %zu byte(s)\n", sizeof(longlongType));
 printf("Size of a float: %zu byte(s)\n", sizeof(floatType));
-  
-  return (0);
+
+return (0);
 }
