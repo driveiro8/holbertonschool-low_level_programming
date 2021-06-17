eab3158f2281c680b65248cdd3daae6512803157
Diego Riveiro <driveiro8@gmail.com>
2021-06-17 11:47:02 -0700

    C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-int main()
+#include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
 {
-  //string initialisation
-  char Mystr[] = "Programming is like building a multilingual puzzle";
-
-  puts(Mystr); //writing the string to stdout
-
-  return 0;
+  puts("\"Programming is like building a multilingual puzzle");
+  return (0);
 }
