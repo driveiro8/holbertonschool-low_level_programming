53207ca1ca0d2e9f4bdd0d128da2f44b5877a117
Diego Riveiro <driveiro8@gmail.com>
2021-06-17 13:00:11 -0700

    C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(void)
+{
+  int intType;
+  long int longType;
+  long long int longlongType
+  float floatType;
+  char charType;
+
+  printf("Size of int: %zu bytes\n", sizeof(intType));
+  printf("Size of char: %zu byte\n", sizeof(longType));
+  printf("Size of float: %zu bytes\n", sizeof(floatType));
+  printf("Size of double: %zu bytes\n", sizeof(longlongType));
+  printf("Size of char: %zu byte\n", sizeof(charType));
+
+  return 0;
+}
