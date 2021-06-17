5120100d8225b8fb6fa4005db139577a4f0d67aa
Diego Riveiro <driveiro8@gmail.com>
2021-06-17 12:32:27 -0700

    C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,6 +4,6 @@
  * Return: 0 if success.
  */
 int main(void)
-{ printf("with proper grammar, but the outcome is a piece of art,");
-  return (0);
+{ printf("with proper grammar, but the outcome is a piece of art,/n");
+return (0);
 }
