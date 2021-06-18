ab3bc25487f7de2f35b149a97423579260ca5281
Diego Riveiro <driveiro8@gmail.com>
2021-06-18 09:58:01 -0700

    if

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (void)
+{
+	char charC, line;
+	line = '\n';
+        for (charC='a'; charC <='z'; charC++)
+	{
+		putchar (charC);
+	}
+
+	putchar(line);
+
+
+return (0);
+
+}
