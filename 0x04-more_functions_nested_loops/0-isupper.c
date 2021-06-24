dc1602b5fcf79b09701db24e74bc96b4dc175000
Diego Riveiro <driveiro8@gmail.com>
2021-06-24 10:37:00 -0700

    More Functions

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _isupper - Check if a letter is upper
+ * @i: The number to be checked
+ *
+ * Return: 1 for upper letter or 0 for any else
+ */
+int _isupper(int i)
+{
+	if (i >= 'a' && i <= 'A')
+	{
+		return (1);
+	}
+
+	return (0);
+}
