febd1a181c736bc92bab393eb87d6aeeb900dc15
Diego Riveiro <driveiro8@gmail.com>
2021-06-28 12:04:25 -0700

    Pointers

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,14 +1,14 @@
 #include "holberton.h"
 /**
  * main - check the code for Holberton School students.
- *
+ *@x: pointer
  * Return: Always 0.
  */
 void swap_int(int *a, int *b)
 {
 	int x;
 
-	x=*a;
+	x = *a;
 	*a = *b;
 	*b = x;
 }
