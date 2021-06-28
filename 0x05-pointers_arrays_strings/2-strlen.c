1bdc9e987fc380a227a45f90cd1e1e572c9c7764
Diego Riveiro <driveiro8@gmail.com>
2021-06-28 12:18:09 -0700

    Pointers

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: String to count
+ *
+ * Return: String length
+ */
+int _strlen(char *s)
+{
+	char *s;
+	int x;
+
+	for (x = 0; *s != '\0'; s++);
+
+	return 0;
+}
