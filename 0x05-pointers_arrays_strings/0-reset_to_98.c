dda573d49279169e05671665c06414b4383a3993
Diego Riveiro <driveiro8@gmail.com>
2021-06-28 11:45:30 -0700

    Pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,10 +1,10 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * reset_to_98 - A function that takes a pointer to an int.
+ *@N: puntero
  * Return: Always 0.
- */
+*/
 void reset_to_98(int *n)
 {
 	*n = 98;
