8a8e2a0a5a641246ff38cb7e8f78589623f1c3e9
Diego Riveiro <driveiro8@gmail.com>
2021-07-01 10:56:18 -0700

    MP

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -11,13 +11,11 @@ char *_strcat(char *dest, char *src)
 	int i;
 	int j;
 
-	for (i = 0; *(dest + i) != '\0'; i++)
-	dest[5] = ' ';
-
-	for (j = 0; *(src + j) != '\0'; j++)
+	for (i = 0; dest[i] != '\0'; i++);
+	for (j = 0; src[j] != '\0'; j++)
 	{
-	*(dest + i) = *(src + j);
-	i++;
+		*(dest + i) = *(src + j);
+		i++;
 	}
 	return (dest);
 }
