8e1a1f8ce03f8fe7d2d47ffed6c09965abc9027a
Diego Riveiro <driveiro8@gmail.com>
2021-07-01 12:02:26 -0700

    MP

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,9 +1,9 @@
 #include "holberton.h"
 /**
- * _strcmp: compares two strings
+ * _strcmp - compares two strings
  * @s1: Source 1
  * @s2: Source 2
- * return: res
+ * Return: int
  **/
 int _strcmp(char *s1, char *s2)
 {
@@ -12,7 +12,7 @@ int _strcmp(char *s1, char *s2)
 	int res;
 
 	j =  0;
-	for(i = 0; *(s1 + i) != '\0'; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 		if (*(s1 + i) == *(s2 + j))
 		{
