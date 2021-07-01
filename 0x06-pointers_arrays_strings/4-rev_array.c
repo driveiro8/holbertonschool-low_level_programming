c5db1212fac4f50dc9084caa4cfe3f24fe43e088
Diego Riveiro <driveiro8@gmail.com>
2021-07-01 13:23:38 -0700

    MP

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * reverse_array - a function that reverses the content of an array of integers
+ * @a: pointer
+ * @n: int
+ * Return: void
+ **/
+void reverse_array(int *a, int n)
+{
+	int i;
+	int h;
+
+	for (i = 0; i < (n / 2); i++)
+	{
+		h = a[i];
+		a[i] = a[n - i - 1];
+		a[n - i - 1] = h;
+	}
+}
