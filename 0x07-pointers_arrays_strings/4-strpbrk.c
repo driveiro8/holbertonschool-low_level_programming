81f4d05aa3a3f0b134cfa86c89810b55f8b0e2ec
Diego Riveiro <driveiro8@gmail.com>
2021-07-05 11:51:17 -0700

    EMP

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -7,6 +7,7 @@
  **/
 char *_strpbrk(char *s, char *accept)
 {
+
 	int i, j;
 
 	for (i = 0; s[i] != '\0'; i++)
@@ -14,9 +15,9 @@ char *_strpbrk(char *s, char *accept)
 		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
-				{
-					return (s + i);
-				}
+			{
+				return (s + i);
+			}
 		}
 	}
 		return ('\0');
