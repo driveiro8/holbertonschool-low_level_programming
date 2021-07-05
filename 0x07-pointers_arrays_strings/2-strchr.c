8c506eaa5f8d95cb698236a1644e27a940208f37
Diego Riveiro <driveiro8@gmail.com>
2021-07-05 09:19:35 -0700

    EMP

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stddef.h>
+/**
+ * _strchr - a function that locates a character
+ * @s: char
+ * @c: char
+ * Return: char
+ **/
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+
+	return ('\0');
+}
