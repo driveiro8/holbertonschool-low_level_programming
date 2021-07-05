7616a83731cd9cbbeb3f631f1ea16523829a6ab3
Diego Riveiro <driveiro8@gmail.com>
2021-07-05 08:40:59 -0700

    EMP

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,13 +2,14 @@
 /**
  * _memcpy - a script that copies memory
  * @dest: char
- * @stc: char
+ * @src: char
  * @n: unsigned int
  * Return: char
  **/
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	unsigned int i;
+
 	for (i = 0; src && i < n; i++)
 		dest[i] = src[i];
 
