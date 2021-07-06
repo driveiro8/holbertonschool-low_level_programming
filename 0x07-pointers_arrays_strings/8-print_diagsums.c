c986bf98aebea4f6485275b180a1992a83f779c0
Diego Riveiro <driveiro8@gmail.com>
2021-07-06 05:19:40 -0700

    EMP

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,23 +1,20 @@
 #include "holberton.h"
 #include <stdio.h>
-
 /**
  * print_diagsums - Print the sum of two diagonals of square matrix
- *
- * @a: the matrix
- * @size: the size
- *
+ * @a: int
+ * @size: int
  * Return: Nothing.
  */
 void print_diagsums(int *a, int size)
 {
-	int b, sum1 = 0, sum2 = 0;
+	int i, x = 0, y = 0;
 
-	for (b = 0; b < size; b++)
+	for (i = 0; i < size; i++)
 	{
-		sum1 += a[(size + 1) * b];
-		sum2 += a[(size - 1) * (b + 1)];
+		x += a[(size + 1) * b];
+		y += a[(size - 1) * (b + 1)];
 	}
 
-	printf("%d, %d\n", sum1, sum2);
+	printf("%d, %d\n", x, y);
 }
